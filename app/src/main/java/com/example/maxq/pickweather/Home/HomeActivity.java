7a2db7d89c705b32d1051b005e0938596e2c5d36
MaxQuero <maxime.quero@gmail.com>
2016-01-10 15:29:01 +0100

    Initial commit + SplashScreen

---
+++ app/src/main/java/com/example/maxq/pickweather/Home/HomeActivity.java
@@ -0,0 +1,18 @@
+package com.example.maxq.pickweather.Home;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.example.maxq.pickweather.R;
+
+public class HomeActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home_activity_main);
+    }
+
+}
