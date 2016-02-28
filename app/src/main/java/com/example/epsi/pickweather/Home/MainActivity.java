fe51ad38f231844e5774eda7d8c08eefd98a8e8a
MaxQuero <maxime.quero@gmail.com>
2016-02-28 19:20:06 +0100

    save 2

--- app/src/main/java/com/example/epsi/pickweather/Home/MainActivity.java
+++ app/src/main/java/com/example/epsi/pickweather/Home/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.epsi.pickweather.Home;
 
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.location.Location;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -13,9 +16,8 @@ import com.example.epsi.pickweather.Home.POJO.CurrentWeather;
 import com.example.epsi.pickweather.R;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
-
-import com.google.android.gms.common.api.GoogleApiClient.*;
-
+import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
+import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
 import com.google.android.gms.location.LocationServices;
 
 import retrofit.Callback;
@@ -25,30 +27,39 @@ import retrofit.android.AndroidLog;
 import retrofit.client.Response;
 
 /**
- * Created by MaxQuero and Camille on 29/01/2016.
+ * Created by MaxQuero on 29/01/2016.
  */
 
 public class MainActivity extends AppCompatActivity implements ConnectionCallbacks, OnConnectionFailedListener {
     GoogleApiClient mGoogleApiClient;
 
-    TextView city, status, humidity, pressure, nameFromLocation;
+    TextView city, status, weather_icon, celcius_icon, humidity, pressure, temp, nameFromLocation;
+    int icon, weatherCode;
     String url = "http://api.openweathermap.org/data/2.5";
     private Toolbar toolbar;                              // Declaring the Toolbar Object
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //Set toolbar as action bar
-        toolbar = (Toolbar) findViewById(R.id.tool_bar); // Attaching the layout to the toolbar object
-        setSupportActionBar(toolbar);
 
 
         city = (TextView) findViewById(R.id.txt_city);
+        weather_icon = (TextView) findViewById(R.id.weather_icon);
         status = (TextView) findViewById(R.id.txt_status);
         humidity = (TextView) findViewById(R.id.txt_humidity);
         pressure = (TextView) findViewById(R.id.txt_press);
         nameFromLocation = (TextView) findViewById(R.id.cityNameFromCoord);
-        RestInterface ri = callAPI(url);
+        temp = (TextView) findViewById(R.id.temp);
+        weather_icon = (TextView) findViewById(R.id.weather_icon);
+        celcius_icon = (TextView) findViewById(R.id.celcius_icon);
+
+        Typeface font = Typeface.createFromAsset(getAssets(), "climacons_webfont.ttf");
+        weather_icon.setTypeface(font);
+        celcius_icon.setTypeface(font);
+        //Set toolbar as action bar
+        toolbar = (Toolbar) findViewById(R.id.tool_bar); // Attaching the layout to the toolbar object
+        setSupportActionBar(toolbar);
+
 
         // Start Google Location API
         if (mGoogleApiClient == null) {
@@ -58,17 +69,23 @@ public class MainActivity extends AppCompatActivity implements ConnectionCallbac
                     .addApi(LocationServices.API)
                     .build();
         }
+        RestInterface ri = callAPI(url);
 
         //Calling method to get weather report from city name
-        ri.getWeatherReportByCityName("Douai", "f48fbd8a004dce121b1720eb6fac9fc7", new Callback<CurrentWeather>() {
+        ri.getWeatherReportByCityName("Quebec", "f48fbd8a004dce121b1720eb6fac9fc7", new Callback<CurrentWeather>() {
             @Override
             public void success(CurrentWeather weather, Response response) {
                 Toast.makeText(getApplicationContext(), String.format("OK"), Toast.LENGTH_SHORT).show();
                 System.out.println(response.toString());
-                city.setText("city :" + weather.getName());
-                status.setText("Status :" + weather.getWeather().get(0).getDescription());
-                humidity.setText("humidity :" + weather.getMain().getHumidity().toString());
-                pressure.setText("pressure :" + weather.getMain().getPressure().toString());
+
+                city.setText(weather.getName() + ", " + weather.getSys().getCountry());
+                //Get simple weather code -> first number says wich type of weather it is
+                status.setText("Current Weather : " + weather.getWeather().get(0).getDescription());
+                humidity.setText("humidity : " + weather.getMain().getHumidity().toString());
+                pressure.setText("pressure : " + weather.getMain().getPressure().toString());
+
+                putWeatherIcons(weather);
+
             }
 
             @Override
@@ -78,6 +95,92 @@ public class MainActivity extends AppCompatActivity implements ConnectionCallbac
                 String merror = error.getMessage();
             }
         });
+
+    }
+
+    public void putWeatherIcons(CurrentWeather weather){
+        weatherCode = weather.getWeather().get(0).getId();
+        double c = weather.getMain().getTemp().intValue() - 273;
+        int celcius_degree = (int) c;
+        View view = weather_icon.getRootView();
+
+        switch (weatherCode){
+            case 500 :
+                icon=R.string.light_rain;
+                weather_icon.setTextColor(Color.parseColor("#0091ea"));
+                view.setBackgroundColor(Color.parseColor("#82b2e4"));
+            case 800:
+                icon = R.string.sunny;
+                weather_icon.setTextColor(Color.parseColor("#fbea2b"));
+                view.setBackgroundColor(Color.parseColor("#0091ea"));
+                break;
+            case 801:
+                icon = R.string.few_clouds;
+                weather_icon.setTextColor(Color.parseColor("#ffffff"));
+                view.setBackgroundColor(Color.parseColor("#9e9e9e"));
+                break;
+            case 905:
+                icon = R.string.windy;
+                weather_icon.setTextColor(Color.parseColor("#ffffff"));
+                view.setBackgroundColor(Color.parseColor("#9e9e9e"));
+                break;
+            default :
+                int simpleId = weatherCode/100;
+                switch (simpleId) {
+                    case 2:
+                        icon = R.string.thunder;
+                        weather_icon.setTextColor(Color.parseColor("#fbea2b"));
+                        view.setBackgroundColor(Color.parseColor("#9e9e9e"));
+
+                        break;
+                    case 3:
+                        icon = R.string.drizzle;
+                        weather_icon.setTextColor(Color.parseColor("#ffffff"));
+                        view.setBackgroundColor(Color.parseColor("#9e9e9e"));
+
+                        break;
+                    case 5:
+                        icon = R.string.rainy;
+                        weather_icon.setTextColor(Color.parseColor("#82b2e4"));
+                        view.setBackgroundColor(Color.parseColor("#9e9e9e"));
+
+                        break;
+                    case 6:
+                        icon = R.string.snowy;
+                        weather_icon.setTextColor(Color.parseColor("#a0dfe4de"));
+                        view.setBackgroundColor(Color.parseColor("#9e9e9e"));
+
+                        break;
+                    case 7 : icon = R.string.foggy;
+                        weather_icon.setTextColor(Color.parseColor("#ffffff"));
+                        view.setBackgroundColor(Color.parseColor("#9e9e9e"));
+
+                        break;
+                    case 8:
+                        icon = R.string.cloudy;
+                        weather_icon.setTextColor(Color.parseColor("#ffffff"));
+                        view.setBackgroundColor(Color.parseColor("#9e9e9e"));
+
+                        break;
+                }
+        }
+        weather_icon.setText(icon);
+        temp.setText(String.valueOf(celcius_degree));
+        celcius_icon.setText(R.string.celcius);
+
+            if (celcius_degree < 10){
+                temp.setTextColor(Color.parseColor("#0091ea"));
+                celcius_icon.setTextColor(Color.parseColor("#0091ea"));
+            } else if (11<celcius_degree & celcius_degree<20){
+                temp.setTextColor(Color.parseColor("#4caf50"));
+                celcius_icon.setTextColor(Color.parseColor("#4caf50"));
+            } else if (21<celcius_degree & celcius_degree<30){
+                temp.setTextColor(Color.parseColor("#e65100"));
+                celcius_icon.setTextColor(Color.parseColor("#e65100"));
+            } else {
+                temp.setTextColor(Color.parseColor("#b71c1c"));
+                celcius_icon.setTextColor(Color.parseColor("#b71c1c"));
+            }
 
     }
 
@@ -130,41 +233,44 @@ public class MainActivity extends AppCompatActivity implements ConnectionCallbac
         mGoogleApiClient.disconnect();
         super.onStop();
     }
-    //Remettre la condition (if) commme avant !!
+
     @Override
     public void onConnected(Bundle connectionHint) {
         Location mLastLocation = LocationServices.FusedLocationApi.getLastLocation(
                 mGoogleApiClient);
-            TextView mLatitudeText, mLongitudeText;
-            mLatitudeText = (TextView) findViewById(R.id.mLatitude);
-            mLongitudeText = (TextView) findViewById(R.id.mLongitude);
-            mLatitudeText.setText(String.valueOf(mLastLocation.getLatitude()));
-            mLongitudeText.setText(String.valueOf(mLastLocation.getLongitude()));
-        String mLatitude;
-        String mLongitude;
-        mLatitude = String.valueOf(mLastLocation.getLatitude());
-        mLongitude = String.valueOf(mLastLocation.getLongitude());
+        if (mLastLocation != null) {
+            String mLatitude, mLongitude;
+            mLatitude = String.valueOf(mLastLocation.getLatitude());
+            mLongitude = String.valueOf(mLastLocation.getLongitude());
 
-        RestInterface ri = callAPI(url);
+            RestInterface ri = callAPI(url);
 
-        ri.getWeatherReportByCoord(mLatitude, mLongitude, "f48fbd8a004dce121b1720eb6fac9fc7", new Callback<CurrentWeather>() {
-            @Override
-            public void success(CurrentWeather weather, Response response) {
-                Toast.makeText(getApplicationContext(), String.format("OK"), Toast.LENGTH_SHORT).show();
-                System.out.println(response.toString());
-                nameFromLocation.setText("Name Location :" + weather.getName());
+            ri.getWeatherReportByCoord(mLatitude, mLongitude, "f48fbd8a004dce121b1720eb6fac9fc7", new Callback<CurrentWeather>() {
+                @Override
+                public void success(CurrentWeather weather, Response response) {
+                   /* Toast.makeText(getApplicationContext(), String.format("OK"), Toast.LENGTH_SHORT).show();
+                    System.out.println(response.toString());
 
-            }
+                    city.setText(weather.getName() + ", " + weather.getSys().getCountry());
+                    //Get simple weather code -> first number says wich type of weather it is
+                    status.setText("Current Weather : " + weather.getWeather().get(0).getDescription());
+                    humidity.setText("humidity : " + weather.getMain().getHumidity().toString());
+                    pressure.setText("pressure : " + weather.getMain().getPressure().toString());
 
-            @Override
-            public void failure(RetrofitError error) {
-                Toast.makeText(getApplicationContext(), String.format("KO"), Toast.LENGTH_SHORT).show();
+                    putWeatherIcons(weather);*/
 
-                String merror = error.getMessage();
-            }
-        });
+                }
+
+                @Override
+                public void failure(RetrofitError error) {
+                    Toast.makeText(getApplicationContext(), String.format("KO"), Toast.LENGTH_SHORT).show();
+
+                    String merror = error.getMessage();
+                }
+            });
+        }
+
     }
-
 
     @Override
     public void onConnectionSuspended(int i) {
